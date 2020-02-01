435841ed58268c558e5bc9540a74e80863060dd6
noordean <ebroyeem90@gmail.com>
2020-02-01 16:42:04 +0100

    Add event page

--- client/app/bundles/HelloWorld/startup/registration.jsx
+++ client/app/bundles/HelloWorld/startup/registration.jsx
@@ -1,8 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
 import HelloWorld from '../components/HelloWorld';
+import EventPage from '../components/EventPage';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   HelloWorld,
+  EventPage
 });
