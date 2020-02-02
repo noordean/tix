0e0a244d166df029f5274a07e9256c1044c120cd
noordean <ebroyeem90@gmail.com>
2020-02-02 11:45:35 +0100

    Add more validation on Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,19 @@ class Event < ApplicationRecord
   belongs_to :user
 
   validates :name, :address, :starts_at, :ends_at, presence: true
+  validate :starts_at_in_the_past
+  validate :ends_at_in_the_past
+  validate :ends_at_greater_than_starts_at
+
+  def starts_at_in_the_past
+    errors.add(:starts_at, "must not be a date in the past") if starts_at.past?
+  end
+
+  def ends_at_in_the_past
+    errors.add(:ends_at, "must not be a date in the past") if ends_at.past?
+  end
+
+  def ends_at_greater_than_starts_at
+    errors.add(:ends_at, "must not be less than start date") if ends_at <= starts_at
+  end
 end
